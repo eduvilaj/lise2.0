ad87281c91eb0707af2da5eca211654d3091d990
eduvilaj <59335450+eduvilaj@users.noreply.github.com>
2020-01-10 09:29:51 -0500

    Update Database file

--- data/database.js
+++ data/database.js
@@ -5,35 +5,176 @@ const data = {
       L1: [
         {
           No_Question: 1,
-          Question: "Quelle est la Superficie d'Haiti ?",
-          Vrai_reponse: 'A',
+          Question:
+            '. Un problème économique auquel est confronté tout individu vient du fait que : ?',
+          Vrai_reponse: 'C',
           reponses: [
-            { Lettre: 'A', possible_reponse: '27 750 kilometre carree' },
-            { Lettre: 'B', possible_reponse: '125 000 kilometre carree' },
-            { Lettre: 'C', possible_reponse: '129 000 kilometre carree' },
-            { Lettre: 'D', possible_reponse: '127 000 kilometre carree' }
+            {
+              Lettre: 'A',
+              possible_reponse:
+                'Les ressources sont mal reparties entre les individus.'
+            },
+            {
+              Lettre: 'B',
+              possible_reponse:
+                'Les besoins sont limités par rapport aux ressources qui sont illimitées'
+            },
+            {
+              Lettre: 'C',
+              possible_reponse:
+                'Les besoins et les désirs dépassent les ressources disponibles pour les satisfaire'
+            },
+            {
+              Lettre: 'D',
+              possible_reponse:
+                'La population mondiale croit beaucoup trop rapidement'
+            }
           ]
         },
         {
           No_Question: 2,
-          Question: "Quelle est la capitale d'Haiti?",
-          Vrai_reponse: 'B',
+          Question: 'Un bien est devenu rare quand :?',
+          Vrai_reponse: 'D',
           reponses: [
-            { Lettre: 'A', possible_reponse: 'Cayes' },
-            { Lettre: 'B', possible_reponse: 'Port-au-Prince' },
-            { Lettre: 'C', possible_reponse: 'Jacmel' },
-            { Lettre: 'D', possible_reponse: 'Petion-Ville' }
+            {
+              Lettre: 'A',
+              possible_reponse:
+                'Il peut satisfaire un besoin pour un nombre important de gens'
+            },
+            {
+              Lettre: 'B',
+              possible_reponse:
+                'Il s’use lentement au fur et à mesure de sa consommation'
+            },
+            {
+              Lettre: 'C',
+              possible_reponse: 'Il permet la production d’autres biens'
+            },
+            {
+              Lettre: 'D',
+              possible_reponse:
+                'Il existe en quantité limité et permet la satisfaction d’un besoin économique'
+            }
           ]
         },
         {
           No_Question: 3,
-          Question: "En Quelle annee , HAiti a t'elle independante?",
+          Question:
+            'Il existe en quantité limité et permet la satisfaction d’un besoin économique?',
+          Vrai_reponse: 'B',
+          reponses: [
+            { Lettre: 'A', possible_reponse: 'Les Cubains' },
+            { Lettre: 'B', possible_reponse: 'Les Haïtiens' },
+            { Lettre: 'C', possible_reponse: 'Les Américains' },
+            { Lettre: 'D', possible_reponse: 'Les Français' }
+          ]
+        },
+        {
+          No_Question: 4,
+          Question:
+            'Les produits qui ont fait objet de rareté en Haïti au début du mois de septembre 2019 sont : ?',
           Vrai_reponse: 'C',
           reponses: [
-            { Lettre: 'A', possible_reponse: '2004' },
-            { Lettre: 'B', possible_reponse: '1992' },
-            { Lettre: 'C', possible_reponse: '1804' },
-            { Lettre: 'D', possible_reponse: '2020' }
+            { Lettre: 'A', possible_reponse: 'Le riz et ses dérivés' },
+            { Lettre: 'B', possible_reponse: 'Les produits laitiers' },
+            { Lettre: 'C', possible_reponse: 'Le pétrole et ses dérivés' },
+            { Lettre: 'D', possible_reponse: 'Les produits pharmaceutiques' },
+            { Lettre: 'E', possible_reponse: 'La viande de poulet' }
+          ]
+        },
+        {
+          No_Question: 5,
+          Question: 'Nos besoins et nos désirs sont illimités parce que :?',
+          Vrai_reponse: 'B',
+          reponses: [
+            { Lettre: 'A', possible_reponse: 'Les ressources sont limitées' },
+            {
+              Lettre: 'B',
+              possible_reponse:
+                'Un besoin satisfait crée toujours un autre besoin'
+            },
+            {
+              Lettre: 'C',
+              possible_reponse:
+                'La redistribution des résultats de la production n’est pas équitable'
+            },
+            {
+              Lettre: 'D',
+              possible_reponse: 'Nous vivons dans un monde de rareté'
+            }
+          ]
+        },
+        {
+          No_Question: 6,
+          Question:
+            'Où est-ce que les individus trouvent de quoi satisfaire leurs besoins ?',
+          Vrai_reponse: 'C',
+          reponses: [
+            { Lettre: 'A', possible_reponse: 'A l’intérieur d’eux-mêmes' },
+            { Lettre: 'B', possible_reponse: 'Dans les livres ' },
+            {
+              Lettre: 'C',
+              possible_reponse: 'Dans la nature (le milieu extérieur)'
+            }
+          ]
+        },
+        {
+          No_Question: 7,
+          Question: 'Quels types de besoins éprouve un enfant en bas âge ?',
+          Vrai_reponse: 'A',
+          reponses: [
+            { Lettre: 'A', possible_reponse: 'Primaires ou vitaux' },
+            {
+              Lettre: 'B',
+              possible_reponse: 'Secondaires qui contribuent à son bien-être'
+            },
+            { Lettre: 'C', possible_reponse: 'Sociaux' }
+          ]
+        },
+        {
+          No_Question: 8,
+          Question: 'Les besoins sociaux d’une personne ont pris naissance :?',
+          Vrai_reponse: 'A',
+          reponses: [
+            {
+              Lettre: 'A',
+              possible_reponse:
+                'Au contact de la collectivité dans laquelle elle aura été élevée'
+            },
+            {
+              Lettre: 'B',
+              possible_reponse: 'Suite à des matraquages publicitaires'
+            },
+            {
+              Lettre: 'C',
+              possible_reponse:
+                'Dès que cette personne commence à gagner de l’argent'
+            }
+          ]
+        },
+        {
+          No_Question: 9,
+          Question:
+            'Pour comprendre l’activité économique il faut la relier :?',
+          Vrai_reponse: 'C',
+          reponses: [
+            { Lettre: 'A', possible_reponse: 'Aux manques' },
+            {
+              Lettre: 'B',
+              possible_reponse:
+                '. Aux choix de politique en matière d’économie des décideurs'
+            },
+            { Lettre: 'C', possible_reponse: 'Aux besoins' },
+            { Lettre: 'D', possible_reponse: 'A la mobilité des agents ' }
+          ]
+        },
+        {
+          No_Question: 10,
+          Question: 'Faire du Sport est un besoin :?',
+          Vrai_reponse: 'B',
+          reponses: [
+            { Lettre: 'A', possible_reponse: 'Physiologique' },
+            { Lettre: 'B', possible_reponse: 'Social' }
           ]
         }
       ]
